38e8dec09bea52909b93dd6bff6b431707d69473
Muthu Raman <110217804+Ronaldo-00@users.noreply.github.com>
2023-11-30 12:18:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,22 @@ function del_Edu_Row(){
     Education_form.style.display= "none";
     EduAdd.style.display = "block"
 }
+
+//top-toogle button display
+
+const mybutton = document.getElementById("top-toggle");
+
+window.onscroll = function() {scrollings()};
+
+function scrollings() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+//to click the top-toggle button its scroll top of the page
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
